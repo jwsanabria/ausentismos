5c9a0080425c780d0c951bf5d81d9f1b9df317fb
John William Sanabria Delgado <john.sanabria@mercadolibre.com.co>
2023-03-19 09:02:46 -0500

    Update filters_web.py

--- ausentismoApp/templatetags/filters_web.py
+++ ausentismoApp/templatetags/filters_web.py
@@ -3,13 +3,14 @@ from django.contrib.humanize.templatetags.humanize import intcomma
 
 register = template.Library()
 
+
 @register.filter
 def prepend_dollars(dollars):
     if dollars:
         dollars = round(float(dollars), 2)
         return "$%s%s" % (intcomma(int(dollars)), ("%0.2f" % dollars)[-3:])
     else:
-        return ''
+        return "$ 0.00"
 
 
 @register.filter
@@ -18,7 +19,8 @@ def prepend_hours(hours):
         hours = hours.strftime("%H:%M")
         return "%s" % (hours)
     else:
-        return ''
+        return ""
+
 
 @register.filter(name="placeholder")
 def placeholder(value, token):
